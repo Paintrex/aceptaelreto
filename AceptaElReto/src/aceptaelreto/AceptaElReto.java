56400c3c368604bc3efb15ace813e9655aac03b4
alvped <alvped@a1417>
2018-01-16 17:10:50 +0100

    Modificacion del archivo principal. 2

--- AceptaElReto/src/aceptaelreto/AceptaElReto.java
+++ AceptaElReto/src/aceptaelreto/AceptaElReto.java
@@ -5,6 +5,7 @@ public class AceptaElReto {
     public static void main(String[] args) {
         
         System.out.println("Acepta el reto!");
+        System.out.println("Sample text");
         
     }
     
